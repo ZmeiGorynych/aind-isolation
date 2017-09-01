6c5a358c485121a1758c96ae23ce606b63571625
Egor Kraev <egor.kraev@gmail.com>
2017-09-01 13:11:35 +0000

    clean up model creation code

--- neural/keras_utils.py
+++ neural/keras_utils.py
@@ -2,6 +2,12 @@ from keras import backend as K
 from keras.engine.topology import Layer
 from keras.initializers import TruncatedNormal
 import numpy as np
+from keras.models import Model, Sequential
+from keras.layers import Input, Lambda, Flatten, Dense, Activation, Dropout
+from keras.layers.merge import Concatenate, Add
+from keras.layers.normalization import BatchNormalization
+from keras import backend as K
+
 from neural.tensorflow_utils import ch_convolve_by_moves, num_biases, num_coeffs, num_fields
 
 class ConvByMoveLayer(Layer):
@@ -35,3 +41,33 @@ class ConvByMoveLayer(Layer):
 
     def compute_output_shape(self, input_shape):
         return (input_shape[0], num_fields, self.out_channels)
+    
+    
+def ResNetLayerFun(x, num_features = 3, mask = None, drop_rate = 0.1):
+    tmp = BatchNormalization()(x)
+    tmp = Activation('relu')(tmp)
+    tmp = ConvByMoveLayer(num_features, mask)(tmp)
+    tmp = BatchNormalization()(tmp)
+    tmp = Activation('relu')(tmp)
+    tmp = ConvByMoveLayer(num_features, mask)(tmp)
+    tmp = Dropout(drop_rate)(tmp)
+    return  Add()([x,tmp])
+
+   
+def deep_model_fun(num_features = 16, num_res_modules = 16, drop_rate = 0.1, activation = 'sigmoid'):
+    player_pos_one_hot = Input(shape = [49, 2])
+    board_state = Input(shape=[49,1])
+    mask = board_state
+    #tmp1 = K.expand_dims(board_state, 2)# TODO: do this in Keras code
+    out = Concatenate()([board_state, player_pos_one_hot])
+    out = ConvByMoveLayer(num_features, mask)(out)
+    for _ in range(num_res_modules):
+        out = ResNetLayerFun(out, num_features, mask, drop_rate)
+    out = Activation('relu')(out)
+    out = Concatenate()([out, player_pos_one_hot])
+    out = Flatten()(out)
+    out = Dense(10, activation = 'relu')(out)
+    out = Dense(1, activation = activation)(out)
+
+    deep_model = Model(inputs = [player_pos_one_hot, board_state], outputs = out)
+    return deep_model
